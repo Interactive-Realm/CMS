67d9ec45950e71c7c972bb72d7ca44946579f5f4
Karsten Finderup Pedersen <kpede22@student.sdu.dk>
2024-12-28 14:50:36 +0000

    Update user avatar

--- frontend/src/lib/components/user/user.tsx
+++ frontend/src/lib/components/user/user.tsx
@@ -93,7 +93,7 @@ export function User({ user, variant = "button" }: UserProps) {
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
                     <AvatarImage src={user.avatar} alt={user.name} />
-                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                    <AvatarFallback className="rounded-lg">{user.name.slice(0, 2).toUpperCase()}</AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
                     <span className="truncate font-semibold">{user.name}</span>
