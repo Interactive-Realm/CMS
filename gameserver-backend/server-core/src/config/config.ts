01d87e4ec9aae8e5d5e0b8ac5b80201cb438bcd3
d3v-tino <zaad1012@outlook.com>
2025-04-29 17:38:53 +0200

    added production env load

--- gameserver-backend/server-core/src/config/config.ts
+++ gameserver-backend/server-core/src/config/config.ts
@@ -3,22 +3,18 @@ import path from 'path';
 
 const env = process.env.NODE_ENV || 'development';
 
-let envPath: string;
+// Build the correct path dynamically
+const envPath = path.resolve(__dirname, `../../.env.${env}`);
 
-if (process.env.NODE_ENV === 'test') {
-  envPath = path.resolve(__dirname, '../../../.env.test');
-} else {
-  envPath = path.resolve(__dirname, `../../.env.${env}`);
-}
-
+// Load the env file
 dotenv.config({ path: envPath });
 
-const config =  {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    CORE_PORT: Number(process.env.CORE_PORT)
+const config = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: Number(process.env.PORT),
+  SUPABASE_KEY: process.env.SUPABASE_KEY,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  CORE_PORT: Number(process.env.CORE_PORT)
 };
 
 export default config;
--- gameserver-backend/server-raffle/src/config/config.ts
+++ gameserver-backend/server-raffle/src/config/config.ts
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: `../../.env.${process.env.NODE_ENV}` || 'test' });
+  dotenv.config({ path: `../../.env.test` });
 } else if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+  dotenv.config({ path: `.env.development` });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: `.env.production` });
+} else {
+  // Default fallback
+  dotenv.config();
 }
 
 const config =  {
